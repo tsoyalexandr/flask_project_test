b355c2eb6cf7c1d389df9b6cd1ea6c2d5c01e21d
tsoyalexandr <tsoy.alexandr93@gmail.com>
2023-04-05 23:28:12 +0600

    end lesson

--- microblog/microblog.py
+++ microblog/microblog.py
@@ -5,6 +5,9 @@ from app.models import User, Post
 # venv\Scripts\activate
 # flask run
 # flask shell
+# users = User.query.all()
+# users
+# set FLASK_DEBUG=0
 
 @app.shell_context_processor
 def make_shell_context():
